5926e23721a0f46605f6a827e181f45691564e94
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-30 11:46:34 +0300

    upgrade spring

--- src/main/java/com/colak/springtutorial/controller/HelloController.java
+++ src/main/java/com/colak/springtutorial/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.colak.springopenapitutorial.controller;
+package com.colak.springtutorial.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
